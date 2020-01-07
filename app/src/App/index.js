35fa55c5c8dfb4e7288b640e86e9b291a16aa623
Kevin Gonzalez <typekev@gmail.com>
2020-01-07 21:15:13 +0100

    Fix unused import: Component in src/App

--- app/src/App/index.js
+++ app/src/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { DrizzleProvider } from '@drizzle/react-plugin';
 import { LoadingContainer } from '@drizzle/react-components';
 
