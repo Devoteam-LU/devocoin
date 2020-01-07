b8b7be52c46d22d1f84b0b65df682f6aba18fce2
Kevin Gonzalez <typekev@gmail.com>
2020-01-07 21:43:56 +0100

    Migrate MyContainer to containers

--- app/src/App/index.js
+++ app/src/App/index.js
@@ -5,7 +5,7 @@ import { LoadingContainer } from '@drizzle/react-components';
 import './App.css';
 
 import drizzleOptions from 'utils/drizzleOptions';
-import MyContainer from 'MyContainer';
+import MyContainer from 'containers/MyContainer';
 
 const App = () => (
   <DrizzleProvider options={drizzleOptions}>
