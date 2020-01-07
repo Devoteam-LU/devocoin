bd1a78e86aed37b4ee894a839969acef6aa5c4c2
Kevin Gonzalez <typekev@gmail.com>
2020-01-07 21:10:34 +0100

    Fix unused import: Component in App

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import { DrizzleProvider } from "@drizzle/react-plugin";
-import { LoadingContainer } from "@drizzle/react-components";
+import React from 'react';
+import { DrizzleProvider } from '@drizzle/react-plugin';
+import { LoadingContainer } from '@drizzle/react-components';
 
-import "./App.css";
+import './App.css';
 
-import drizzleOptions from "./drizzleOptions";
-import MyContainer from "./MyContainer";
+import drizzleOptions from './drizzleOptions';
+import MyContainer from './MyContainer';
 
 const App = () => (
   <DrizzleProvider options={drizzleOptions}>
