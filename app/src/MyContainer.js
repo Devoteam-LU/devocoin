2fef7f8afd92f3144dd0468ecf9e39476d88f30b
Kevin Gonzalez <typekev@gmail.com>
2020-01-07 21:42:13 +0100

    Migrate MyComponent to components

--- app/src/MyContainer.js
+++ app/src/MyContainer.js
@@ -1,5 +1,5 @@
-import MyComponent from "./MyComponent";
 import { drizzleConnect } from "@drizzle/react-plugin";
+import MyComponent from "components/MyComponent";
 
 const mapStateToProps = state => {
   return {
