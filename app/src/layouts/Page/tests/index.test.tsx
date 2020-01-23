22912790f1c80a895c615735b5dd777749d89922
Kevin Gonzalez <typekev@gmail.com>
2020-01-23 23:31:20 +0100

    Remove unused import

--- app/src/layouts/Page/tests/index.test.tsx
+++ app/src/layouts/Page/tests/index.test.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from 'layouts/Page';
-import { BrowserRouter as Router } from 'react-router-dom';
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Page title="title">test</Page>, div);
