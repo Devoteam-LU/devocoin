dc5ca314a1a6adbb78eea25f247ee5b40aec73fe
Kevin Gonzalez <typekev@gmail.com>
2020-01-24 21:48:16 +0100

    Reorder imports

--- app/src/layouts/Navigation/tests/index.test.tsx
+++ app/src/layouts/Navigation/tests/index.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navigation from 'layouts/Navigation';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Navigation from 'layouts/Navigation';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
