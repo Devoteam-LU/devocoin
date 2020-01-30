443d8c91ffba1b0fc46b614075b5c570dd181ccf
Kevin Gonzalez <typekev@gmail.com>
2020-01-30 23:46:20 +0100

    Add getUser mock api

--- app/src/common/types.ts
+++ app/src/common/types.ts
@@ -1,5 +1,18 @@
 import { ReactNode } from 'react';
 
+export enum UserGroup {
+    Manager = "MANAGER",
+    Officer = "OFFICER",
+    Consultant = "CONSULTANT",
+}
+
+export interface User {
+    id: string;
+    userGroup: UserGroup;
+    firstName: string;
+    lastName: string;
+}
+
 export interface Obligation {
     title: string;
     description: string;
---
+++ app/src/mocks/api/getUser.ts
@@ -0,0 +1,6 @@
+import officerUser from 'mocks/json/officerUser.json'
+import { User } from 'common/types';
+
+const getUser = (): User => officerUser as User;
+
+export default getUser;
