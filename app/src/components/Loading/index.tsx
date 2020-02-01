9a94f35a7290021facd8259204725c4ad052988b
Kevin Gonzalez <typekev@gmail.com>
2020-02-01 23:06:29 +0100

    Use IonLoading in Loading

--- app/src/components/Loading/index.tsx
+++ app/src/components/Loading/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { drizzleReactHooks } from '@drizzle/react-plugin';
-
-const { useDrizzleState } = drizzleReactHooks;
+import { IonLoading } from '@ionic/react';
+import { useDrizzleState } from 'utils/drizzleHooks';
 
 interface Props {
   children: React.ReactNode;
 }
 
-const Loading = ({ children }: Props): JSX.Element => {
-  const { drizzleStatus } = useDrizzleState((drizzleState: any) => drizzleState);
-  return <>{drizzleStatus.initialized ? children : '...loading'}</>;
+const Loading = ({ children }: Props) => {
+  const { drizzleStatus } = useDrizzleState();
+
+  return (
+    <>
+      <IonLoading isOpen={!drizzleStatus.initialized} message={'Connecting to the network...'} />
+      {drizzleStatus.initialized && children}
+    </>
+  );
 };
 
 export default Loading;
