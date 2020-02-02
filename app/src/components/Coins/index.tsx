d2354958f90e0b62cd549091b82d7b3f683cfae3
Kevin Gonzalez <typekev@gmail.com>
2020-02-02 20:53:39 +0100

    Style Coins component with ion card

--- app/src/components/Coins/index.tsx
+++ app/src/components/Coins/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonItem } from '@ionic/react';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Coins = ({ children }: Props) => {
-  return <>{children}</>;
+  return (
+    <IonCard color="success">
+      <IonCardHeader>
+        <IonCardSubtitle>Devocoins</IonCardSubtitle>
+      </IonCardHeader>
+      <IonItem button color="success" href="#">
+        <IonCardContent>
+          <h1>
+            <strong>{`${children} Ð`}</strong>
+          </h1>
+        </IonCardContent>
+      </IonItem>
+    </IonCard>
+  );
 };
 
 export default Coins;
