faf776ed9590fd7be93f79928882a35b8d646369
bacharif <arif.bachir@hotmail.com>
2020-02-02 15:59:13 +0100

    first draft bottom navagation bar

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,31 +1,62 @@
 import React from 'react';
-import { IonContent, IonItemOption, IonIcon } from '@ionic/react';
+import { IonRouterOutlet } from '@ionic/react';
+import { Route } from 'react-router';
+import { IonContent, IonItemOption, IonIcon, IonTabs, IonTabBar, IonTabButton, IonLabel, IonBadge, IonAvatar } from '@ionic/react';
 import getObligations from 'mocks/api/getObligations';
 import Page from 'layouts/Page';
 import ObligationsList from 'components/ObligationsList';
-import { archive } from 'ionicons/icons';
+import { archive, addCircle, send, search, ribbon } from 'ionicons/icons';
 
 const Home = () => {
-  const obligations = getObligations();
-  return (
-    <Page title="Home">
-      <IonContent>
-        <ObligationsList
-          obligations={obligations}
-          startOption={
-            <IonItemOption color="danger" onClick={() => {}}>
-              <IonIcon slot="start" icon={archive} size="large" />
+    const obligations = getObligations();
+    return (
+        <Page title="Home">
+            <IonContent>
+                <ObligationsList
+                    obligations={obligations}
+                    startOption={
+                        <IonItemOption color="danger" onClick={() => { }}>
+                            <IonIcon slot="start" icon={archive} size="large" />
+                        </IonItemOption>
+                    }
+                    endOption={
+                        <IonItemOption color="success" onClick={() => { }}>
+                            Complete
             </IonItemOption>
-          }
-          endOption={
-            <IonItemOption color="success" onClick={() => {}}>
-              Complete
-            </IonItemOption>
-          }
-        />
-      </IonContent>
-    </Page>
-  );
+                    }
+                />
+            </IonContent>
+            <IonTabs>
+                <IonRouterOutlet>
+
+                </IonRouterOutlet>
+                <IonTabBar slot="bottom">
+                    <IonTabButton tab="ribbon">
+                        <IonIcon icon={ribbon} size="large" />
+                        <IonBadge>6</IonBadge>
+                    </IonTabButton>
+
+                    <IonTabButton tab="search">
+                        <IonIcon icon={search} size="large"  />
+                  
+                    </IonTabButton>
+
+                    <IonTabButton tab="add">
+                        <IonIcon icon={addCircle} size="large" />
+                    </IonTabButton>
+
+                    <IonTabButton tab="send">
+                        <IonIcon icon={send} size="large" />
+                    </IonTabButton>
+                    <IonTabButton tab="profile">
+                        <IonAvatar>
+                            <img src="../../../assets/logo.png" />
+                        </IonAvatar>
+                    </IonTabButton>
+                </IonTabBar>
+            </IonTabs>
+        </Page>
+    );
 };
 
 export default Home;
