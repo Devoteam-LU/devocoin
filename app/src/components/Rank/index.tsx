0506171bfbb5247d801d6f8e967a7b04b0124da9
Kevin Gonzalez <typekev@gmail.com>
2020-02-02 20:53:46 +0100

    Style Rank component with ion card

--- app/src/components/Rank/index.tsx
+++ app/src/components/Rank/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonItem } from '@ionic/react';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Rank = ({ children }: Props) => {
-  return <>{children}</>;
+  return (
+    <IonCard color="tertiary">
+      <IonCardHeader>
+        <IonCardSubtitle>Rank</IonCardSubtitle>
+      </IonCardHeader>
+      <IonItem button color="tertiary" href="#">
+        <IonCardContent>
+          <h1>
+            <strong>{children}</strong>
+          </h1>
+        </IonCardContent>
+      </IonItem>
+    </IonCard>
+  );
 };
 
 export default Rank;
