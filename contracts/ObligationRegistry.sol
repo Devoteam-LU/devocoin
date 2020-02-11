67453f2c0d934d95addb907089d938a63746b632
Kevin Gonzalez <typekev@gmail.com>
2020-02-11 23:45:58 +0100

    Rename terms in ObligationRegistry

--- contracts/ObligationRegistry.sol
+++ contracts/ObligationRegistry.sol
@@ -3,16 +3,16 @@ pragma solidity >=0.4.21 <0.7.0;
 import "./Obligation.sol";
 
 contract ObligationRegistry {
-    mapping(address => address) public userContracts;
+    mapping(address => address) public obligations;
 
-    function newInstanceFunc(
+    function create(
         address _address,
         uint256 _reward,
         address payable _beneficiary
     ) public returns (address) {
         Obligation T = new Obligation(_reward, _beneficiary);
         address deployedAddress = address(T);
-        userContracts[_address] = deployedAddress;
+        obligations[_address] = deployedAddress;
         return deployedAddress;
     }
 }
