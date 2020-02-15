73463d5950cf2a28648d579201a4ccbe1ddb42a0
Kevin Gonzalez <typekev@gmail.com>
2020-02-15 23:19:26 +0100

    Add get and create obligations funcs for ObligationReg

--- app/src/pages/Obligations/OfficerObligations.tsx
+++ app/src/pages/Obligations/OfficerObligations.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { IonItemOption } from '@ionic/react';
 import { newContextComponents } from '@drizzle/react-components';
-import getObligationsPendingApproval from 'mocks/api/getObligationsPendingApproval';
 import ObligationsList from 'components/ObligationsList';
 import { useDrizzle, useDrizzleState } from 'utils/drizzleHooks';
 import { DrizzleContractForm } from 'common/types';
 import ConsultantObligations from './ConsultantObligations';
 
-const { ContractForm } = newContextComponents;
+const { ContractForm, ContractData } = newContextComponents;
+
+const getObligations = async (drizzle: any, drizzleState: any, setObligations: any) => {
+  const obligations = await drizzle.contracts.ObligationRegistry.methods
+    .getObligationsByBeneficiary(drizzleState.accounts[0])
+    .call();
+  setObligations(obligations);
+};
+
+const createObligation = async (drizzle: any, drizzleState: any, setAddress: any) => {
+  const obligations = await drizzle.contracts.ObligationRegistry.methods
+    .createObligation(3, drizzleState.accounts[0])
+    .call();
+  setAddress(obligations);
+};
 
 const OfficerObligations = () => {
   const { drizzle } = useDrizzle();
   const drizzleState = useDrizzleState();
+  const [obligations, setObligations] = useState<any[]>([]);
+  const [address, setAddress] = useState('');
 
-  const obligations = getObligationsPendingApproval();
+  useEffect(() => {
+    getObligations(drizzle, drizzleState, setObligations);
+  }, [address, drizzle, drizzleState]);
+
+  useEffect(() => {
+    createObligation(drizzle, drizzleState, setAddress);
+  }, [drizzle, drizzleState]);
+
   return (
     <ConsultantObligations>
       <ObligationsList
@@ -51,7 +73,14 @@ const OfficerObligations = () => {
         drizzle={drizzle}
         drizzleState={drizzleState}
         contract="ObligationRegistry"
-        method="create"
+        method="createObligation"
+      />
+      <ContractData
+        drizzle={drizzle}
+        drizzleState={drizzleState}
+        contract="ObligationRegistry"
+        method="getObligationsByBeneficiary"
+        methodArgs={[drizzleState.accounts[0]]}
       />
     </ConsultantObligations>
   );
